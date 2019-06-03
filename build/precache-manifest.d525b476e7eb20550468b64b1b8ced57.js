70964e89df95ec598c4e36ff8a00e132c4da3c24
Morgan Dorfman <mdorfman21@gmail.com>
2019-06-03 00:02:10 -0400

    production

---
+++ build/precache-manifest.d525b476e7eb20550468b64b1b8ced57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4696cb79c58192e532fc620bd51b05",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b2454e05287f9ed8227a",
+    "url": "/clicky-game/static/css/main.2247d2e0.chunk.css"
+  },
+  {
+    "revision": "ada989ee15f1d2950fe9",
+    "url": "/clicky-game/static/js/2.58c46b89.chunk.js"
+  },
+  {
+    "revision": "b2454e05287f9ed8227a",
+    "url": "/clicky-game/static/js/main.927c512b.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
